119652cf4325a7d5f80f3f712d2dec7f403acbe9
David W.H. Swenson <dwhs@hyperblazer.net>
2017-06-29 12:16:52 +0200

    Simple develop

--- devtools/ops-conda-recipe/build.sh
+++ devtools/ops-conda-recipe/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo "Starting build.sh for OPS"
-pwd
-ls
 # this step is done automatically by conda build
 # cp -r $RECIPE_DIR/../.. $SRC_DIR
-$PYTHON setup.py develop --single-version-externally-managed --record=/tmp/record.txt
+$PYTHON setup.py develop #--single-version-externally-managed --record=/tmp/record.txt
 
 # Add more build steps here, if they are necessary.
 

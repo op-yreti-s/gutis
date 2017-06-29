7ebe5407c423746704f900a8e840c43367d06bbb
David W.H. Swenson <dwhs@hyperblazer.net>
2017-06-29 11:37:48 +0200

    Debug info in build.sh

--- devtools/ops-conda-recipe/build.sh
+++ devtools/ops-conda-recipe/build.sh
@@ -3,7 +3,9 @@
 # this step is done automatically by conda build
 # cp -r $RECIPE_DIR/../.. $SRC_DIR
 #$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
-
+echo "Starting build.sh for OPS"
+pwd
+ls
 pip install -e .
 
 # Add more build steps here, if they are necessary.

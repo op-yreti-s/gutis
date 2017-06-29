2ae59a4c1bc06940bc5f6b20ae14754bfc981369
David W.H. Swenson <dwhs@hyperblazer.net>
2017-06-29 11:55:44 +0200

    Switch back to setup.py from pip install -e .

--- devtools/ops-conda-recipe/build.sh
+++ devtools/ops-conda-recipe/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# this step is done automatically by conda build
-# cp -r $RECIPE_DIR/../.. $SRC_DIR
-#$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
 echo "Starting build.sh for OPS"
 pwd
 ls
-pip install -e .
+# this step is done automatically by conda build
+# cp -r $RECIPE_DIR/../.. $SRC_DIR
+$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
 
 # Add more build steps here, if they are necessary.
 

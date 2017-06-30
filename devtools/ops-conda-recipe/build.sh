6fd6bd3fc193facdac231260e37de9bf85fe02b0
David W.H. Swenson <dwhs@hyperblazer.net>
2017-06-30 11:48:42 +0200

    Should now be a working OPS build?

--- devtools/ops-conda-recipe/build.sh
+++ devtools/ops-conda-recipe/build.sh
@@ -2,7 +2,8 @@
 
 # this step is done automatically by conda build
 # cp -r $RECIPE_DIR/../.. $SRC_DIR
-$PYTHON setup.py develop #--single-version-externally-managed --record=/tmp/record.txt
+echo $PYTHON
+$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
 
 # Add more build steps here, if they are necessary.
 
